699ec9bd2a359ad6a70acaa9b283c8af8bbfb365
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-08-01 00:09:06 +0500

    suspense on working

--- suspense/src/app/page.tsx
+++ suspense/src/app/page.tsx
@@ -1,21 +1,54 @@
-
-
+'use client'
+import { useState } from "react";
+import { Suspense } from "react";
 export default function Home() {
-  const fruits:string[]=['Apple','Bannana','Mango']
+const [state,setState]=useState('false');
+
+
+
+
+const before=(
+  <button onClick={()=>setState('true')} >Click Me</button>
+)
+
+const after=(
+  <div className="w-full h-full flex items-center justify-center bg-yellow-300">
+  <div>
+   <p className="font-bold">Fruits List</p>
+   <ul>
+    
+    <Suspense fallback={<h2>Loading....</h2>}>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    <li>Apple</li>
+    </Suspense>
+
+   </ul>
+    </div>
+</div>
+ 
+)
+  
   return (
     <>
-    <button>Click Me</button>
-  <div className="w-full h-screen flex items-center justify-center flex-row bg-yellow-300">
-    <div>
-     <p className="font-bold">Hello World!</p>
-     <ul>
-      {fruits.map((key:string)=>(
-      <li>{key}</li>)
-      )}
-     </ul>
-      </div>
-    
-  </div>
+{state==='false'?before:after}
 </>
   )
 }
+
