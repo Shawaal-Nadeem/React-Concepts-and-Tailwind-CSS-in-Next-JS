6d7ce0d4dfeba91395acbc2876b34e9e2ffcd6c4
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-08-02 17:12:15 +0500

    State Management

--- state-management/src/app/page.tsx
+++ state-management/src/app/page.tsx
@@ -24,7 +24,8 @@ setResult('Correct Answer');
     <p className=" mt-10 ml-3 font-bold">What is the name of Shawaal's crush?</p>
     <textarea placeholder={answer} onChange={(e)=>{setAnswer(e.target.value)}} disabled={state==='true'} className={state==='true'?' opacity-50 pl-1 border border-black border-solid ml-3 mt-4':' pl-1 border border-black border-solid ml-3 mt-4'} ></textarea>
     <br/>
-<button onClick={HandleDisablingAndCheckAnswering} disabled={state==='true' ||answer==='Answer'} className={state==='true' ||answer==='Answer'?' opacity-50 border border-black ml-3 mt-2 bg-black text-white w-16':' border border-black ml-3 mt-2 bg-black text-white w-16'}>Submit</button>
+<button onClick={HandleDisablingAndCheckAnswering} disabled={state==='true' ||answer==='Answer'} className={state==='true' ||answer==='Answer'?' opacity-50 border border-black ml-3 mt-2 bg-black text-white w-16':' border border-black ml-3 mt-2 bg-black text-white w-16 rounded-md'}>Submit</button>
+<a href=''><button className=" border border-blue-500 bg-blue-500 text-white ml-9 rounded">Reload</button></a>
     <br/>
     <br/>
     <b className={result==='Correct Answer'?' text-green-500 ml-3':' text-red-600 ml-3'}>{result}</b>
