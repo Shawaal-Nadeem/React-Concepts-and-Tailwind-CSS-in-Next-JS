5764609ae9e72e5247fb99b01985295b682ec304
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-08-02 16:51:27 +0500

    State Management

---
+++ state-management/src/app/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { useState } from "react"
+
+
+export default function Home() {
+  const [answer,setAnswer]=useState('Answer');
+  const [state,setState]=useState('false');
+  const [result,setResult]=useState('');
+  function HandleDisablingAndCheckAnswering(){
+    setState('true');
+    if(answer.toLowerCase()==='nancy'){
+setTimeout(()=>{alert(`Yes!!! ${answer} is Crush of Shawaal`)},1500)
+setResult('Correct Answer');
+    }
+    else
+    {
+      setResult('Wrong Answer');
+    }
+  }
+
+  return (
+    <>
+    <h2 className=" text-center font-extrabold text-3xl">Crush Guess Quizz</h2>
+    <p className=" mt-10 ml-3 font-bold">What is the name of Shawaal's crush?</p>
+    <textarea placeholder={answer} onChange={(e)=>{setAnswer(e.target.value)}} disabled={state==='true'} className={state==='true'?' opacity-50 pl-1 border border-black border-solid ml-3 mt-4':' pl-1 border border-black border-solid ml-3 mt-4'} ></textarea>
+    <br/>
+<button onClick={HandleDisablingAndCheckAnswering} disabled={state==='true' ||answer==='Answer'} className={state==='true' ||answer==='Answer'?' opacity-50 border border-black ml-3 mt-2 bg-black text-white w-16':' border border-black ml-3 mt-2 bg-black text-white w-16'}>Submit</button>
+    <br/>
+    <br/>
+    <b className={result==='Correct Answer'?' text-green-500 ml-3':' text-red-600 ml-3'}>{result}</b>
+    </>
+    )
+}
--- suspense/src/app/page.tsx
+++ suspense/src/app/page.tsx
@@ -1,66 +1,21 @@
-'use client'
-import { useState } from "react";
-import { Suspense } from "react";
-
 
 
 export default function Home() {
-const [state,setState]=useState('false');
-const [fruit,setFruit]=useState('false');
-
-function temp()
-{
-  setTimeout(()=>{setFruit('true')},3000)
-}
-const listOfFruits=(
-  <>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  <li>Apple</li>
-  </>
-)
-
-const before=(
-  <button onClick={()=>setState('true')} >Click Me</button>
-)
-
-const after=(
-  <div className="w-full h-full flex items-center justify-center bg-yellow-300">
-  <div>
-   <p className="font-bold">Fruits List</p>
-   <ul>
-    <Suspense fallback={<h2>Loading....</h2>}>
-    <ul onLoad={temp} {...fruit==='true'?listOfFruits:null}>
-
-    </ul>
-    </Suspense>
-
-   </ul>
-    </div>
-</div>
- 
-)
-  
+  const fruits:string[]=['Apple','Bannana','Mango']
   return (
     <>
-{state==='false'?before:after}
+    <button>Click Me</button>
+  <div className="w-full h-screen flex items-center justify-center flex-row bg-yellow-300">
+    <div>
+     <p className="font-bold">Hello World!</p>
+     <ul>
+      {fruits.map((key)=>(
+      <li></li>)
+      )}
+     </ul>
+      </div>
+    
+  </div>
 </>
   )
 }
-
