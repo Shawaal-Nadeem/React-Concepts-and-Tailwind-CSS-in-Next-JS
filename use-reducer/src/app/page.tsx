bb3a2e1187dc2407d65d463ae3ef201d8a4e1cfe
Shawaal Nadeem <hafizshawalnadeem@gmail.com>
2023-08-19 23:52:10 +0500

    updated

--- use-reducer/src/app/page.tsx
+++ use-reducer/src/app/page.tsx
@@ -9,6 +9,7 @@ import { useReducer } from "react"
 // if(action.type==='increase')
 // {
 //   return {
+//     ...state,
 //     counter:state.counter+1,
 //   }
 // } 
@@ -16,17 +17,20 @@ import { useReducer } from "react"
 // {
 //   if(state.counter>0){
 //     return{
+//       ...state,
 //       counter:state.counter-1,
 //     }
 //   }
 //   else{
 //     return{
+//       ...state,
 //       counter:state.counter,
 //     }
 //   }
 // }
 // else if(action.type==='reset'){
 //   return{
+//     ...state,
 //     counter:0,
 //   }
 // }
@@ -49,31 +53,40 @@ import { useReducer } from "react"
 
 // Ahsaan Code
 
+
+const initialState = { age: 0, height: 0 };
+
 const reducer = (state:any, action:any) => {
-  if (action.type == 'increment')
-      return { age: state.age + 1, height: state.height + 2 }
-  else if (action.type == 'decrement') {
-      if (state.age > 0)
-          return { age: state.age - 1, height: state.height - 2 }
-      else return { age: state.age = 0, height: state.height }
+  switch (action.type) {
+    case 'increment':
+      return { ...state, age: state.age + 1, height: state.height + 2 };
+    case 'decrement':
+      if (state.age > 0) {
+        return { ...state, age: state.age - 1, height: state.height - 2 };
+      } else {
+        return { ...state, age: 0, height: state.height };
+      }
+    case 'reset':
+      return { ...state, age: 0, height: 0 };
+    default:
+      return state;
   }
-  else if (action.type == 'reset')
-      return { age: state.age = 0, height: state.height = 0 }
-}
+};
 
 const Counter = () => {
-  const [state, dispatch] = useReducer(reducer, {age:0,height:0})
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-      <div className="main">
-          <div className="buttons">
-              <button onClick={() => { dispatch({ type: 'increment' }) }}>Increment</button>
-              <button onClick={() => { dispatch({ type: 'decrement' }) }}>Decrement</button>
-              <button onClick={() => { dispatch({ type: 'reset' }) }}>Reset</button>
-          </div>
-          <h1>Hello! You are {state.age}</h1>
-          <h2>Your Height is {state.height} cm</h2>
+    <div className="main">
+      <div className="buttons">
+        <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+        <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+        <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
       </div>
-  )
-}
-export default Counter
+      <h1>Hello! You are {state.age}</h1>
+      <h2>Your Height is {state.height} cm</h2>
+    </div>
+  );
+};
+
+export default Counter;
